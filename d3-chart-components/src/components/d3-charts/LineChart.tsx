74e2692a264752a0a76b9462ab64dbddd39695a3
Yogesh Datir <yogdat@gmail.com>
2021-06-20 23:11:32 +0530

    Added tooltip

--- d3-chart-components/src/components/d3-charts/LineChart.tsx
+++ d3-chart-components/src/components/d3-charts/LineChart.tsx
@@ -14,7 +14,7 @@ const LineChart = ({
   dimensions = {
     height: 500,
     width: 800,
-    margin: { top: 30, right: 30, bottom: 90, left: 40 },
+    margin: { top: 30, right: 30, bottom: 90, left: 60 },
   },
   data,
 }: Props) => {
@@ -23,15 +23,15 @@ const LineChart = ({
   const { height, width, margin } = dimensions;
 
   const containerWidth: number =
-    (width || 0) - (margin.right || 0) - (margin.left || 0);
+    (width || 800) - (margin.right || 30) - (margin.left || 60);
   const containerHeight: number =
-    (height || 0) - (margin.top || 0) - (margin.bottom || 0);
-  console.log(containerWidth, containerHeight);
+    (height || 500) - (margin.top || 30) - (margin.bottom || 90);
 
   useEffect(() => {
     const parseDate = d3.timeParse("%d-%m-%Y");
 
-    const xAccessor = (dataMember: any) => parseDate(dataMember.date) || 0;
+    const xAccessor = (dataMember: any) =>
+      parseDate(dataMember.date) || new Date();
     const yAccessor = (dataMember: any) => parseInt(dataMember.adj_close);
     const draw = () => {
       const svg = d3
@@ -47,6 +47,22 @@ const LineChart = ({
         .append("g")
         .style("transform", `translate(${margin.left}px, ${margin.top}px)`);
 
+      const tooltip: any = d3
+        .select("#tooltip")
+        // if not applied, hovering on tooltip makes it disappear
+        .style("pointer-events", "none");
+
+      const tooltipDot = ctr
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "#fc8781")
+        .attr("stroke", "black")
+        .attr("stroke-width", 2)
+        .style("opacity", 0)
+        .style("pointer-events", "none");
+
+      const tooltipLine = ctr.append("line");
+
       // Scales
       const xDomainExtents: any = d3.extent(data, xAccessor);
       const yDomainExtents: any = d3.extent(data, yAccessor);
@@ -57,7 +73,7 @@ const LineChart = ({
       const xScale = d3
         .scaleUtc()
         .domain(xDomainExtents)
-        .range([margin.left || 0, containerWidth]);
+        .range([0, containerWidth]);
 
       const yScale = d3
         .scaleLinear()
@@ -79,9 +95,9 @@ const LineChart = ({
         .attr("fill", "none")
         .attr("stroke", "#30475e");
 
-      const monthParse = d3.timeFormat("%b %y");
-
       // X axis
+
+      const monthParse = d3.timeFormat("%b %y");
       const xAxis = d3.axisBottom(xScale).tickFormat((d: any) => {
         // console.log(monthParse(d));
         return monthParse(d);
@@ -109,13 +125,64 @@ const LineChart = ({
 
       // Y axis
 
-      const yAxis = d3.axisLeft(yScale);
+      const yAxis = d3.axisLeft(yScale).tickFormat((d: any) => `₹${d}`);
+
+      ctr.append("g").call(yAxis).classed("axis", true);
+
+      //Tooltip
 
       ctr
-        .append("g")
-        .call(yAxis)
-        .style("transform", `translateX(${margin.left}px)`)
-        .classed("axis", true);
+        .append("rect")
+        .attr("width", containerWidth)
+        .attr("height", containerHeight)
+        .style("opacity", 0)
+        .on("touchmouse mousemove", function (event: any) {
+          const mousePos = d3.pointer(event, this);
+          const date = xScale.invert(mousePos[0]);
+
+          const bisector = d3.bisector(xAccessor).left;
+          const index = bisector(data, date);
+          const stock = data[index - 1];
+
+          tooltipDot
+            .style("opacity", 1)
+            .attr("cx", xScale(xAccessor(stock)))
+            .attr("cy", yScale(yAccessor(stock)))
+            .raise();
+
+          tooltipLine
+            .attr("x1", xScale(xAccessor(stock)))
+            .attr("y1", containerHeight)
+            .attr("x2", xScale(xAccessor(stock)))
+            .attr("y2", 0)
+            .classed("mean-line", true);
+
+          tooltip
+            .style("display", "block")
+            .style("top", yScale(yAccessor(stock)) - 50 + "px")
+            .style(
+              "left",
+              xScale(xAccessor(stock) || new Date()) +
+                (margin.left || 0) -
+                tooltip["_groups"][0][0].clientWidth / 2 +
+                "px"
+            );
+
+          tooltip.select(".price").text(`₹${yAccessor(stock)}`);
+
+          const dateFormatter = d3.timeFormat("%B %-d, %Y");
+
+          tooltip
+            .select(".date")
+            .text(`${dateFormatter(xAccessor(stock) || new Date())}`);
+
+          // console.log(stock);
+        })
+        .on("mouseleave", function (event: any) {
+          tooltipDot.style("opacity", 0);
+          tooltip.style("display", "none");
+          tooltipLine.classed("mean-line", false);
+        });
     };
     data && draw();
   }, [
@@ -130,7 +197,14 @@ const LineChart = ({
     width,
   ]);
 
-  return <div id="chart" ref={ref}></div>;
+  return (
+    <div id="chart" className="chart" ref={ref}>
+      <div id="tooltip">
+        <div className="price"></div>
+        <div className="date"></div>
+      </div>
+    </div>
+  );
 };
 
 export default LineChart;
